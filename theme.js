cbde9bb430282afc34a3d72bee3f176d72fe66c4
Pragya Pandya <pragyapandya.108@gmail.com>
2026-04-25 00:41:50 -0400

    updated all pages to display dark mode

--- theme.js
+++ theme.js
@@ -1,23 +1,19 @@
-// Grab the toggle button and the body element
 const themeToggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
 
-// 1. CHECK MEMORY ON PAGE LOAD
-// When any page loads, check if the user previously saved a 'dark' preference
+// when pg loads check if user already toggled dark mode
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme === 'dark') {
   body.classList.add('dark-mode');
 }
 
-// 2. LISTEN FOR CLICKS
-// If the button exists on the page, listen for a click
+// if  button exists on the page, listen for a click
 if (themeToggleBtn) {
   themeToggleBtn.addEventListener('click', () => {
     // Toggle the dark mode class on and off
     body.classList.toggle('dark-mode');
 
-    // 3. SAVE TO MEMORY
-    // Check if the body now has the dark mode class, and save that choice
+    // Check if the body now has the dark mode class and save choice for future clicks/navigation
     if (body.classList.contains('dark-mode')) {
       localStorage.setItem('theme', 'dark');
     } else {
